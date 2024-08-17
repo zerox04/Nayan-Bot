ff26a5ffa7ce502eb55296f50676ed854e23e8b3
zerox04 <itzfakebyriyad@gmail.com>
2024-08-17 11:49:10 +0600

    Update Ana.js

--- scripts/commands/Ana.js
+++ scripts/commands/Ana.js
@@ -6,7 +6,7 @@ module.exports.config = {
   hasPermssion: 0,
   credits: "riyadxRubish",
   description: "better than all Sim simi",
-  prefix:true,
+  prefix: true,
   commandCategory: "ChatBots",
   cooldowns: 5,
 };
